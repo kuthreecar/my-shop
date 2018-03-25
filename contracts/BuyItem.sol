aefaa303fbabaff4bc826cf5f103d590d1f2f4e4
kuthreecar <xccchung@gmail.com>
2018-03-25 11:17:19 +0800

    modified contract

--- contracts/BuyItem.sol
+++ contracts/BuyItem.sol
@@ -1,40 +1,82 @@
 pragma solidity ^0.4.17;
 
 contract BuyItem {
-
+struct Item
+{
+  uint itemID;
+  string itemName;
+  string itemDescription;
+  string itemPicLoc;
+  uint itemPrice;
+  uint itemAvailiable;
+}
 // variables
 uint[] itemId;
-mapping(uint => uint) public itemPrice;
-mapping(uint => uint) public itemAvailiable;
+mapping(uint => Item) public itemByID; //id -> item
+
+Item[4] items;
 
 
-function initItem(uint[] _itemId, uint[] _itemPrice, uint[] _itemAvailable) public returns (uint)
+function BuyItem() public
 {
-  itemId = _itemId;
-  for(uint i = 0; i < itemId.length;i++)
+  items[0].itemID=0;
+  items[0].itemName="Black Bag";
+  items[0].itemPicLoc="images/blackhandbag.jpg";
+  items[0].itemDescription = "Black Bag";
+  items[0].itemPrice = 20;
+  items[0].itemAvailiable =3;
+
+  items[1].itemID=1;
+  items[1].itemName="Black Handbag";
+  items[1].itemPicLoc="images/brownhandbag.jpg";
+  items[1].itemDescription = "Black Handbag";
+  items[1].itemPrice = 30;
+  items[1].itemAvailiable =7;
+
+  items[2].itemID=2;
+  items[2].itemName="Rocksack";
+  items[2].itemPicLoc="images/rocksack.jpg";
+  items[2].itemDescription = "Rocksack";
+  items[2].itemPrice = 20;
+  items[2].itemAvailiable =4;
+
+  items[3].itemID=3;
+  items[3].itemName="Black Bag";
+  items[3].itemPicLoc="images/blackbag.jpg";
+  items[3].itemDescription = "Black Bag";
+  items[3].itemPrice = 15;
+  items[3].itemAvailiable =5;
+
+  uint arrayLength = items.length;
+
+  for(uint i = 0; i < arrayLength;i++)
   {
-    itemPrice[_itemId[i]]= _itemPrice[i];
-    itemAvailiable[_itemId[i]] = _itemAvailable[i];
+    itemId.push(items[i].itemID);
+    itemByID[items[i].itemID]=items[i];
   }
-  return 1;
 }
 
-function buy(uint id,uint quantity) payable public returns (uint) 
+function buy(uint id,uint quantity) payable public returns (uint)
 {
 
   require(checkValidItem(id));
-  require(itemAvailiable[id] >= quantity);
+  require(itemByID[id].itemAvailiable >= quantity);
 
-  itemAvailiable[id] -= quantity;
-  return itemAvailiable[id];
+  itemByID[id].itemAvailiable -= quantity;
+  return itemByID[id].itemAvailiable;
 
 
 }
-
-function getItemAvailable(uint id) view public returns (uint) 
+function getItemIDList() view public returns (uint[])
 {
-  return itemAvailiable[id];
+  return itemId;
 }
+
+function getItemData(uint _itemID) view public returns (uint, string, string, string, uint, uint)
+{
+  return (itemByID[_itemID].itemID, itemByID[_itemID].itemName, itemByID[_itemID].itemPicLoc,itemByID[_itemID].itemDescription,itemByID[_itemID].itemPrice,itemByID[_itemID].itemAvailiable);
+}
+
 
 function checkValidItem(uint id) view public returns(bool)
 {
@@ -48,6 +90,4 @@ function checkValidItem(uint id) view public returns(bool)
   return false;
 }
 
-
 }
-
--- test/TestBuyItem.sol
+++ test/TestBuyItem.sol
@@ -9,61 +9,37 @@ contract TestBuyItem {
   uint[] itemPrice;
   uint[] itemAvailiable;
   BuyItem buyItem = BuyItem(DeployedAddresses.BuyItem());
-
-// Test Init
-function testInitItem() public {
-
-  itemId.push(1);
-  itemId.push(2);
-  itemId.push(3);
-  itemId.push(4);
-  itemPrice.push(2);
-  itemPrice.push(3);
-  itemPrice.push(4);
-  itemPrice.push(5);
-  itemAvailiable.push(3);
-  itemAvailiable.push(4);
-  itemAvailiable.push(5);
-  itemAvailiable.push(6);
-  
-  uint returnedId = buyItem.initItem(itemId, itemPrice, itemAvailiable);
-
-  uint expected = 1;
-
-  Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded.");
+string aa;
+function testCheckValidItem() public {
+  bool returned = buyItem.checkValidItem(2);
+  bool expected = true;
+  Assert.equal(returned, expected, "CheckValidItem Fail");
 }
 
-// Test GetItemAvailable
-function testGetItemAvailable() public {
-
-  uint returnedId = buyItem.getItemAvailable(3);
-
-  uint expected = 5;
-
-  Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded.");
+function testCheckValidItem2() public {
+  bool returned = buyItem.checkValidItem(5);
+  bool expected = false;
+  Assert.equal(returned, expected, "CheckValidItem2 Fail");
 }
 
-// Test buy if available
-function testBuy() public {
-
-  uint returnedId = buyItem.buy(3,2);
-
-  uint expected = 3;
-
-  Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded.");
+function testGetItemData() public {
+string memory a;string memory b;string memory c;
+ uint d;uint e;
+(a,b,c,d,e) = buyItem.getItemData(2);
+  aa = "Rocksack";
+  Assert.equal(a, aa, "testGetItemData Fail");
 }
 
-
-  
 // Test buy if not available
 // A little utility that runs a wrapped method invocation as an internal Solidity call
 // Returns true if the underlying call succeeds and false if it throws.
+/*
 function execute(string signature) internal returns (bool){
     bytes4 sig = bytes4(keccak256(signature));
     address self = address(this);
     return self.call(sig);
   }
-  
+
 function buyTooMany() public {
     buyItem.buy(3,5);
 }
@@ -71,4 +47,5 @@ function buyTooMany() public {
 function testBuy2() public {
   Assert.isFalse(execute('buyTooMany()'), "Should fail over limit");
 }
+*/
 }
